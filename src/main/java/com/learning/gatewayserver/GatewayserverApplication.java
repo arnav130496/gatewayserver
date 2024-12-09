3ee5d5a1b3a5c23e5bbe2eef0200d10609a2bc39
arnav130496 <arnav130496@gmail.com>
2024-12-09 16:35:19 +0530

    compatible versions

--- src/main/java/com/learning/gatewayserver/GatewayserverApplication.java
+++ src/main/java/com/learning/gatewayserver/GatewayserverApplication.java
@@ -2,12 +2,39 @@ package com.learning.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayserverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayserverApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayserverApplication.class, args);
+    }
+
+    @Bean
+    RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p ->
+                        p.path("/learning/accountsms/**")
+                                .filters(f ->
+                                        f.rewritePath("/learning/accountsms/(?<segment>.*)", "/${segment}")
+                                                .addResponseHeader("X-RESPONSE-TIME", LocalDateTime.now().toString()))
+                                .uri("lb://ACCOUNTS"))
+                .route(p ->
+                        p.path("/learning/cardsms/**")
+                                .filters(f -> f.rewritePath("/learning/cardsms/(?<segment>.*)", "/${segment}"))
+                                .uri("lb://CARDS"))
+                .route(p ->
+                        p.path("/learning/loansms/**")
+                                .filters(f -> f.rewritePath("/learning/loansms/(?<segment>.*)", "/${segment}"))
+                                .uri("lb://LOANS"))
+                .build();
+    }
 
 }
